9689ffa98c89f030b89c7712725e812b4e5c20af
sta <stasd82@yandex.ru>
2022-06-28 13:37:57 +0300

    Change RouteFunc type and add initial documentation

--- tux.go
+++ tux.go
@@ -9,7 +9,8 @@ import (
 	"github.com/uptrace/bunrouter"
 )
 
-type RouteFunc func(context.Context, http.ResponseWriter, *http.Request) error
+// The Route type is an adapter to allow the use of our advanced functions as HTTP handlers.
+type Route func(context.Context, http.ResponseWriter, *http.Request) error
 
 type Tux struct {
 	*bunrouter.CompatRouter
@@ -23,16 +24,18 @@ func New(c chan os.Signal) *Tux {
 	}
 }
 
+// Finish is used to gracefully shutdown our race when an integrity issue is identified.
 func (t *Tux) Finish() {
 	t.shutdown <- syscall.SIGTERM
 }
 
-func (t *Tux) AddRoute(verb string, group string, path string, rf RouteFunc) {
+// AddRoute registers the route (handler) function for the given pattern.
+func (t *Tux) AddRoute(verb string, group string, path string, route Route) {
 
 	h := func(w http.ResponseWriter, r *http.Request) {
 
 		ctx := context.TODO()
-		if err := rf(ctx, w, r); err != nil {
+		if err := route(ctx, w, r); err != nil {
 			t.Finish()
 			return
 		}

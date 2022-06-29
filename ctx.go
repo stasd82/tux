b820e8e8aa80b43378a0ca0120795fbbac3a5722
sta <stasd82@yandex.ru>
2022-06-29 13:50:56 +0300

    Add get/set operations for Context

---
+++ ctx.go
@@ -0,0 +1,42 @@
+package tux
+
+import (
+	"context"
+	"errors"
+	"time"
+)
+
+type keyCtx int
+
+const key keyCtx = 1
+
+type Values struct {
+	TraceID    string
+	Now        time.Time
+	StatusCode int
+}
+
+func GetValues(ctx context.Context) (*Values, error) {
+	v, ok := ctx.Value(key).(*Values)
+	if !ok {
+		return nil, errors.New("web value missing from context")
+	}
+	return v, nil
+}
+
+func GetTraceID(ctx context.Context) string {
+	v, ok := ctx.Value(key).(*Values)
+	if !ok {
+		return "00000000-0000-0000-000000000000"
+	}
+	return v.TraceID
+}
+
+func SetStatusCode(ctx context.Context, statusCode int) error {
+	v, ok := ctx.Value(key).(*Values)
+	if !ok {
+		return errors.New("web value missing from context")
+	}
+	v.StatusCode = statusCode
+	return nil
+}

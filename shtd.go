0d01f6821569ce4ebd30a6b31a45a2af10eb4676
sta <stasd82@yandex.ru>
2022-07-01 01:08:27 +0300

    Implement shutdownError type

---
+++ shtd.go
@@ -0,0 +1,20 @@
+package tux
+
+import "errors"
+
+type shutdownError struct {
+	Message string
+}
+
+func NewShutdownError(msg string) error {
+	return &shutdownError{Message: msg}
+}
+
+func (s *shutdownError) Error() string {
+	return s.Message
+}
+
+func IsShutdown(err error) bool {
+	var se shutdownError
+	return errors.Is(err, &se)
+}
